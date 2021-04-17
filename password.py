8506bf13ac40a8f9b71280f9c023aa5e8807794a
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2021-04-16 23:39:52 -0700

    complete implementation of password function

--- password.py
+++ password.py
@@ -1,5 +1,11 @@
+import random
+
 def password(length):
     return ''.join([random_char() for i in range(length)])
 
 def random_char():
-    return 'a'
+    # Characters to use in passwords.
+    CHARS = "abcdefghijklmnopqrstuvwxyz" + \
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + \
+            "1234567890!@#$%^&*()"
+    return CHARS[random.randrange(0, len(CHARS))]
